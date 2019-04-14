a2e6b846ca71b92aa7552adce2d436d95197d5a5
TheShadow88 <stef4oben88@gmail.com>
2019-04-14 17:48:22 +0300

    Simplified code, improved with media queries

--- responsive.js
+++ responsive.js
@@ -1,24 +1,9 @@
 $(document).ready(function(){
 
 	       if ($("html").width() <= 700 ){
-	       		$("#navDrop").show()
 				$("#NavBar").removeClass("navbar-left").addClass("fixed-top navbar")
 				$("#navbarCollapse").removeClass("navbar-nav").addClass("navbar-collapse collapse")
 	       }
-	       else{
-	       	$("#navDrop").hide()
-	    
-	       	// if($('#navbar').hasClass("fixed-top")){
-	       	// 	$('#navbar').removeClass("fixed-top")
-		       // 	$('#navbar').removeClass("navbar")
-		       // 	$('#navbar').addClass("navbar-left")
-		       
-	       	// }
-
-	       }
-
-	       
-
 	      
 
 }); 
