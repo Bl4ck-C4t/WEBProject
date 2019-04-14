d293284ca28c3d46d1cf068bbdd750db6a215aeb
TheShadow88 <stef4oben88@gmail.com>
2019-04-14 21:32:43 +0300

    Adding Intro and font to the first page

--- responsive.js
+++ responsive.js
@@ -1,9 +1,8 @@
 $(document).ready(function() {
 
 	       if ($("html").width() <= 700){
+               $(".bar-heading-holder").css("background", "none");
 				$("#NavBar").removeClass("navbar-left").addClass("fixed-top navbar")
 				$("#navbarCollapse").removeClass("navbar-nav").addClass("navbar-collapse collapse")
 	       }
-	      
-
 }); 
