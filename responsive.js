4cc6b16ff6bb6d2158a6e9646d40ed7c12b88393
TheShadow88 <stef4oben88@gmail.com>
2019-04-14 19:08:38 +0300

    Little changes here and there :3

--- responsive.js
+++ responsive.js
@@ -1,6 +1,6 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-	       if ($("html").width() <= 700 ){
+	       if ($("html").width() <= 700){
 				$("#NavBar").removeClass("navbar-left").addClass("fixed-top navbar")
 				$("#navbarCollapse").removeClass("navbar-nav").addClass("navbar-collapse collapse")
 	       }
