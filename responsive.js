1bf6833b4227437c5b3a60f5240c0c4983f9c20d
TheShadow88 <stef4oben88@gmail.com>
2019-04-15 14:58:09 +0300

    Changed media query boundries to match default bootstrap ones

--- responsive.js
+++ responsive.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-	       if ($("html").width() <= 700){
+	       if ($("html").width() <= 576){
                $(".bar-heading-holder").css("background", "none");
 				$("#NavBar").removeClass("navbar-left").addClass("fixed-top navbar")
 				$("#navbarCollapse").removeClass("navbar-nav").addClass("navbar-collapse collapse")
