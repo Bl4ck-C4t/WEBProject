12e39be22cf4c3820c3b0695597ad72847cbc8e5
TheShadow88 <stef4oben88@gmail.com>
2019-04-18 10:32:36 +0300

    Fixed a bug with the coloring, where the same string gets matched twice

--- color-code.js
+++ color-code.js
@@ -1,50 +1,133 @@
 $(document).ready(function() {
+
 	function* matchAll(reg, text){
 		var array1;
 		while ((array1 = reg.exec(text)) !== null) {
 			  yield array1;
 			}
 	}
-	var codes = $(".code-block")
-	var vars = ["char", "int", "bool", "string", "float"]
-	// let coloring_dict = {"css-vars": vars, "css-strings": string}
-	var mod_text;
-	var text;
-	var array1;
-	var allMatches = [];
-	var regex1;
-	codes.each(function( index ) {
-		text = $(this).text();
-		vars.forEach(function(el){ // color vars
-			// text = text.replace(new RegExp(`\\b${el}`, 'g'), `<span class="var var-${el}">$&</span>`)
-		
-			for (const array1 of matchAll(new RegExp(`\\b${el}`, 'g'), text)){
-				console.log(`Found ${array1[0]}. Next starts at ${array1.index}.`);
-				array1.class = `var var-${el}`;
-				allMatches.push(array1)
-			}
-		})
-		
-		for (const array1 of matchAll(new RegExp(`\".*?\"`, 'g'), text)){
-			console.log(`Found ${array1[0]}. Next starts at ${array1.index}.`);
-			array1.class = "string";
-			allMatches.push(array1)
-		}
-		// console.log(allMatches)
 
-		allMatches = allMatches.sort((a,b) => a.index - b.index)
+	// function replaceAllMatches2(allMatches, text){ // Legacy
+	// 	var new_text = 	""
+	// 	var match = 0;
+	// 	for (var i = 0; i < text.length; i++) {
+	// 		var m = allMatches[match]
+	// 		if (i == m.index){
+
+ 
+	// 			new_text += `<span class="${m.class}">${m[0]}</span>`
+	// 			console.log(new_text, m)
+	// 			i += m[0].length-1 // maybe -1
+	// 			match += 1
+	// 			if(match == allMatches.length){
+	// 				new_text += text.substring(i+1)
+	// 				console.log(new_text, m)
+	// 				break;
+	// 			}
+	// 		}
+	// 		else{
+	// 			new_text += text[i]
+	// 		}	
+			
+	// 	}
+	// 	return new_text
+	// }
+
+	function replaceAllMatches(allMatches, text){
+		var new_text = 	""
+		var match = 0;
+		var last_pos = 0
+		for (const m of allMatches) {
+			new_text += text.substring(last_pos, m.index)
+			new_text += `<span class="${m.class}">${m[0]}</span>`
+			last_pos = m.index + m[0].length			
+		}
+		new_text += text.substring(last_pos)
+		return new_text
+	}
+		
+
+
+	function matchType(reg, text, class_type){
+		var matches = []
+		for (const array1 of matchAll(new RegExp(reg, 'g'), text)){
+				// console.log(`Found ${array1[0]}. Next starts at ${array1.index}.`);
+				array1.class = class_type;
+				matches.push(array1)
+		}
+		return matches
+	}
+
+	function matchTypes(arr, class_t, connected=false){
+		var allMatches = [];
+		arr.forEach(function(el){ // color vars
+			var reg = `\\b${el}\\b`
+			if(connected){
+				el = el.split('').map((c) => "\\"+c).join('')
+				reg = `${el}`
+			}
+			allMatches = allMatches.concat(matchType(reg, text, `${class_t} ${class_t}-${el}`))
+			
+		})
+		return allMatches;
+	}
+
+	function isIndexin(allMatches, index, pos){
+		if(pos == 0){
+			return true;
+		}
+		for (var i = 0; i < pos; i++) {
+			var m = allMatches[i]
+			// console.log(`Comparing index ${index} with ${m}`)
+			if(index >= m.index && index < m.index+m[0].length-1){
+				// console.log("Index is false")
+				return false;
+			}
+		}
+		// console.log("Index is true")
+		return true;
+	}
+
+	var vars = ["char", "int", "bool", "string", "float"]
+	var res_words = ["if", "else", "while", "for", "elseif", "return", "func", "in"]
+	var def_functions = ["print", "input", "map", "filter", "push"]
+	var operators = ["&&", "||", "!", "+", "=", "-", "*", "/", "^", "%", "<", ">", "."]
+	// var punctuation = ",;(){}[]".split('')
+	var punctuation = "()[]".split('')
+
+	var codes = $(".code-block")
+	var text;
+	var allMatches;
+	codes.each(function( index ) {
+		allMatches = [];
+		text = $(this).text();
+		// vars.forEach(function(el){ // color vars
+			
+		// 	allMatches = allMatches.concat(matchType(`\\b${el}\\b`, text, `var var-${el}`))
+			
+		// })
+		allMatches = allMatches.concat(matchTypes(punctuation, "punctuation", true))
+		allMatches = allMatches.concat(matchTypes(vars, "var"))
+		allMatches = allMatches.concat(matchTypes(res_words, "reserved"))
+		allMatches = allMatches.concat(matchTypes(def_functions, "function"))
+		
+		allMatches = allMatches.concat(matchType("\\b(True|False)\\b", text, "boolean"))
+		// allMatches = allMatches.concat(matchType("	\\b[a-zA-Z_][a-zA-Z_0-9]*(?= *\\()", text, "function"))
+
+		// allMatches = allMatches.concat(matchType("\\b[a-zA-Z_][a-zA-Z_0-9]", text, "symbol"))	
+		allMatches = allMatches.concat(matchTypes(operators, "operator", true))
+		allMatches = allMatches.concat(matchType("\\b-?\\d+(\\.\\d+)?", text, "number"))
+		allMatches = allMatches.concat(matchType(`'\\w'`, text, "character"))
+		allMatches = allMatches.concat(matchType(`\".*?\"`, text, "string"))
+
+		allMatches = allMatches.sort((a,b) => a.index - b.index) // sort matches by index ascending
+		allMatches = allMatches.filter((match, index) => isIndexin(allMatches, match.index, index)); // remove equal elements
 
 
 		console.log("Ordered:")
 		console.log(allMatches)
-		var new_text = 	text.substring(0, allMatches[0].index)
-		allMatches.forEach((m) => {
-		    new_text += text.substring(m.index).replace(m[0], `<span class="${m.class}">${m[0]}</span>`)
-		})
-
-
-		// text = text.replace(new RegExp(`\".*?\"`, 'g'), `<span class="string">$&</span>`)
-		$(this).html(text)
+		new_text = replaceAllMatches(allMatches, text)
+		$(this).html(new_text)
 			
 	});
 
