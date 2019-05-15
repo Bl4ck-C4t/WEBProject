114188d058f8c6e9fed511a3d0f77816e1434f75
TheShadow88 <stef4oben88@gmail.com>
2019-05-15 12:07:26 +0300

    More content

--- responsive.js
+++ responsive.js
@@ -1,11 +1,11 @@
 function resizer(){
 	if ($("html").width() <= 576){
-           $(".bar-heading-holder").css("background", "none");
+            $(".bar-heading-holder").css("background", "none");
 			$("#NavBar").removeClass("navbar-left").addClass("fixed-top navbar")
 			$("#navbarCollapse").removeClass("navbar-nav").addClass("navbar-collapse collapse")
     }
     else if($("#NavBar").hasClass("fixed-top")){
-    	$(".bar-heading-holder").css("background", "#2980b9");
+    	    $(".bar-heading-holder").css("background", "#2980b9");
 			$("#NavBar").removeClass("fixed-top navbar").addClass("navbar-left")
 			$("#navbarCollapse").removeClass("navbar-collapse collapse").addClass("navbar-nav")
     }
